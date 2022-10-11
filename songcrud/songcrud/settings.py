b764e647db7a1b5b796d773fdfb65a462ece22fc
bolexs <boluwatufeadeojo@gmail.com>
2022-10-11 01:11:18 +0100

    hid secret key

--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uzrwo%%a05yp40_-vjz3$rv26*&q)cn($y&3hf95$ap(1313d+'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
