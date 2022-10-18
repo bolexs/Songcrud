601c7167495fbf5d270e976e37ae9768cc26008f
bolexs <boluwatufeadeojo@gmail.com>
2022-10-18 23:14:35 +0100

    added models to admin

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Artist,Song,Lyrics
+
+
+admin.site.register(Artist)
+admin.site.register(Song)
+admin.site.register(Lyrics)
