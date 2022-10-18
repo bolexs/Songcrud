db2960415934a31a149a29e6970467ae46d059db
bolexs <boluwatufeadeojo@gmail.com>
2022-10-18 22:18:36 +0100

    added models and attributes

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,22 @@
+from importlib.resources import contents
+from operator import mod
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    first_name = models.CharField(max_length= 40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    title = models.CharField(max_length= 40)
+    data_released = models.DateField()
+    likes = models.IntegerField(max_length = 10)
+    artist_id = models.ForeignKey(Artist, on_delete = models.CASCADE)
+
+
+class Lyrics(models.Model):
+    content = models.CharField(max_length = 1000)
+    sond_id = models.ForeignKey(Song, on_delete = models.CASCADE)
