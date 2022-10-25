a15e08fb588ea255c976c9f579b68be02db613ec
bolexs <boluwatufeadeojo@gmail.com>
2022-10-25 13:31:57 +0100

    worked on getting lyrics

--- songcrud/musicapp/serializers.py
+++ songcrud/musicapp/serializers.py
@@ -1,24 +1,24 @@
 from rest_framework import serializers
 from .models import Lyric, Song
 
-# class LyricsSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Lyric
-#         fields = ['content']
+class LyricsSerializer(serializers.ModelSerializer):
+     class Meta:
+         model = Lyric
+         fields = ['content']
 
 
 class SongSerializer(serializers.ModelSerializer):
-    # lyrics = serializers.SerializerMethodField('get_lyrics')
-    artist_id = serializers.StringRelatedField(read_only=True)
+     lyrics = serializers.SerializerMethodField('get_lyrics')
+     artist_id = serializers.StringRelatedField(read_only=True)
 
-    # def get_lyrics(self, instance):
-    #     query = instance.lyric_set.all()
-    #     lyrics = LyricsSerializer(query).data
-    #     return lyrics
+     def get_lyrics(self, instance):
+         query = instance.lyric_set.all()
+         lyrics = LyricsSerializer(query, many=True).data
+         return lyrics
 
-    class Meta:
+     class Meta:
         model = Song
-        fields = ['id', 'artist_id', 'title', 'date_released','likes' ]
+        fields = ['id', 'artist_id', 'title', 'date_released','likes', 'lyrics' ]
 
 class UpdateSongSerializer(serializers.ModelSerializer):
 
